449d2ef112bcf43cf2aea5cf6ccffa1fdfe6b508
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2025-05-09 00:19:25 +0200

    add button component

---
+++ client/src/components/Button.ts
@@ -0,0 +1,39 @@
+// src/components/Button.ts
+import * as PIXI from 'pixi.js';
+
+export class Button extends PIXI.Container {
+    private background: PIXI.Graphics;
+    private textLabel: PIXI.Text;
+
+    constructor(text: string, width = 200, height = 60) {
+        super();
+
+        this.eventMode = 'static';
+        this.cursor = 'pointer';
+
+        this.background = new PIXI.Graphics()
+            .roundRect(0, 0, width, height, 10)
+            .fill(0xf5f5f5);
+
+        this.addChild(this.background);
+
+        this.textLabel = new PIXI.Text({
+            text,
+            style: {
+            fill: 0x000000,
+            fontSize: 24,
+            align: 'center',
+            },
+        });
+
+        this.textLabel.anchor.set(0.5);
+        this.textLabel.x = width / 2;
+        this.textLabel.y = height / 2;
+        this.addChild(this.textLabel);
+
+    }
+
+    public onClick(fn: () => void) {
+        this.on('pointerdown', fn);
+    }
+}
